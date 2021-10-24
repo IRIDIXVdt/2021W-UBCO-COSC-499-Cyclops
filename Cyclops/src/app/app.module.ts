5214f8414927c26059277877e0dddd2a1961ca55
Sumeet <sumeet-bajwa-34@live.ca>
2021-10-24 14:30:56 -0700

    Update Slides.ts and AppModules.ts file
    Imported SlidesComponent, and SwiperModule into appmodule
    
    Updated SlidesComponent to contain code for slides

--- Cyclops/src/app/app.module.ts
+++ Cyclops/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidesComponent } from './slides/slides.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,SlidesComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SwiperModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, SlidesComponent],
 })
 export class AppModule {}
--- Cyclops/src/app/slides/slides.component.ts
+++ Cyclops/src/app/slides/slides.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import { IonicSwiper } from '@ionic/angular';
+
+SwiperCore.use([IonicSwiper, Navigation, Pagination]);
+
 
 @Component({
   selector: 'app-slides',
   templateUrl: './slides.component.html',
   styleUrls: ['./slides.component.scss'],
 })
-export class SlidesComponent implements OnInit {
+export class SlidesComponent {
+
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
