8b9413e19aef5925734994ec5f8ad39acafb96de
Sumeet <sumeet-bajwa-34@live.ca>
2021-10-24 14:42:06 -0700

    tab3.ts and tab3.html
    swiper module added to tab3 and html for slides to tab3

--- Cyclops/src/app/tab3/tab3.page.ts
+++ Cyclops/src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import { IonicSwiper } from '@ionic/angular';
+
+SwiperCore.use([IonicSwiper, Navigation, Pagination]);
 
 @Component({
   selector: 'app-tab3',
