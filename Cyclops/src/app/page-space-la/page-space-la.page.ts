ef2ecba88ec43cf3de9c1faf89a2cbb12127fab4
IRIDIXVdt <larry_gu@126.com>
2021-11-20 21:51:22 +0800

    bug fixed

--- Cyclops/src/app/page-space-la/page-space-la.page.ts
+++ Cyclops/src/app/page-space-la/page-space-la.page.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-space-la.page.scss'],
 })
 export class PageSpaceLaPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  constructor() { 
   }
 
+  ngOnInit() {
+    const searchbar = document.querySelector('ion-searchbar');
+    const items = Array.from(document.querySelector('ion-list').children as HTMLCollectionOf<HTMLElement>);
+
+    searchbar.addEventListener('ionInput', handleInput);
+
+    function handleInput(event) {
+      const query = event.target.value.toLowerCase();
+      requestAnimationFrame(() => {
+        items.forEach(item => {
+          const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
+          item.style.display = shouldShow ? 'block' : 'none';
+        });
+      });
+    }
+  }
+  
 }
+// const searchbar = document.querySelector('ion-searchbar');
+// const items = Array.from(document.querySelector('ion-list').children);
+
+// searchbar.addEventListener('ionInput', handleInput);
+
+// function handleInput(event) {
+//   const query = event.target.value.toLowerCase();
+//   requestAnimationFrame(() => {
+//     items.forEach(item => {
+//       const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
+//       item.style.display = shouldShow ? 'block' : 'none';
+//     });
+//   });
+// }
