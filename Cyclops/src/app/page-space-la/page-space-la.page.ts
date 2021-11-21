a3f66c2bdd8aee66cc18af341613d1d073ec2219
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-11-21 22:37:41 +0800

    Improved Search Bar

--- Cyclops/src/app/page-space-la/page-space-la.page.ts
+++ Cyclops/src/app/page-space-la/page-space-la.page.ts
@@ -11,6 +11,10 @@ export class PageSpaceLaPage implements OnInit {
   }
 
   ngOnInit() {
+    const searchResult = document.querySelector('#requested') as HTMLElement;
+    searchResult.style.display = 'none';
+
+    const textSpace = document.querySelector('#origin') as HTMLElement;
     const searchbar = document.querySelector('ion-searchbar');
     const items = Array.from(document.querySelector('ion-list').children as HTMLCollectionOf<HTMLElement>);
 
@@ -25,20 +29,28 @@ export class PageSpaceLaPage implements OnInit {
         });
       });
     }
+
+    searchbar.addEventListener('ionFocus', handleFocus);
+    
+    function handleFocus(){
+      console.log('message Focus emit');
+      requestAnimationFrame(() => {
+        textSpace.style.display = 'none';
+        searchResult.style.display = 'block';
+      }); 
+      
+    }
+
+    
+    searchbar.addEventListener('ionBlur',handleBlur);
+    function handleBlur() {
+      console.log('message Blur emit');
+      requestAnimationFrame(() => {
+        textSpace.style.display = 'block';
+        searchResult.style.display = 'none';
+      }); 
+    }
   }
-  
 }
-// const searchbar = document.querySelector('ion-searchbar');
-// const items = Array.from(document.querySelector('ion-list').children);
 
-// searchbar.addEventListener('ionInput', handleInput);
 
-// function handleInput(event) {
-//   const query = event.target.value.toLowerCase();
-//   requestAnimationFrame(() => {
-//     items.forEach(item => {
-//       const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
-//       item.style.display = shouldShow ? 'block' : 'none';
-//     });
-//   });
-// }
