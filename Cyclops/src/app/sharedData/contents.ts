949946ee311da35412e3afe1fa6e59ed168bb41a
lilmergo <megan.nguyenn261@gmail.com>
2021-11-22 11:22:59 -0800

    Add articles in content
    Main sections:
    - food
    - household appliances
    Bonus:
    - Industry

--- Cyclops/src/app/sharedData/contents.ts
+++ Cyclops/src/app/sharedData/contents.ts
@@ -21,11 +21,40 @@ export const CONTENTS: content[] = [
 
     {
         id: '2',
-        title: 'Fossil Fuels1',
+        title: 'Fossil Fuels',
         subtitle: '',
         image: '../assets/pic3.jpg',
         article: 'While the next few sections of ‘Laying the Foundation’ are focused on energy, it’s important to understand that polluting energy sources are just one part of the problem. We’ll go over numerous examples in the main sections – but for now, just remember that it’s not only about how we produce energy – it’s also about what we use it for.'
         
+    },
+
+    {
+        id: '3',
+        title: 'Food',
+        subtitle: '',
+        image: '../assets/pic7.jpg',
+        article: 'In 2018, food systems accounted for roughly 33% of our global GHG emissions [note that many reports have more cautious estimates, like the IPCC’s 21-37% range]. Unsurprisingly, the farming stage – where crops or animals are grown – is responsible for the majority of our food system’s emissions.'
+        
+    },
+
+    {
+        id: '4',
+        title: 'Household Appliances',
+        subtitle: '',
+        image: '../assets/pic8.jpg',
+        article: 'Household appliances are often sneaky energy consumers, far less scrutinized than HVAC systems. It’s important to understand that anything that heats, cools, lights up, or moves needs energy. For the residential sector, most of that energy is supplied in the form of electricity or natural gas.'
+        
+    },
+
+    {
+        id: '5',
+        title: 'Industry',
+        subtitle: '',
+        image: '../assets/pic9.jpg',
+        article: 'Industry was responsible for 24% of the world’s CO2 emissions in 2018 and accounts for 19% of global freshwater withdrawals, while industrial waste generation is over 17 times higher than municipal solid waste.'
+        
     }
+
+
 ]
    
