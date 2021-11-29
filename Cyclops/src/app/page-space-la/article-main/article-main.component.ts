ea69bf7ff9b007b40a379d46ce89b730f4ea6a56
IRIDIXVdt <larry_gu@126.com>
2021-11-30 02:00:20 +0800

    Content title and intro allow for edit

--- Cyclops/src/app/page-space-la/article-main/article-main.component.ts
+++ Cyclops/src/app/page-space-la/article-main/article-main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { displayArticle } from '../../sharedData/displayArticle';
+import { displayArticles } from '../../sharedData/displayArticles';
 import { ArticleModalMainComponent } from '../article-modal-main/article-modal-main.component';
 import { ModalController } from '@ionic/angular';
 
@@ -12,39 +13,38 @@ export class ArticleMainComponent implements OnInit {
   @Input()
   articleComponent: displayArticle[];
 
-  articleId;
+  contents: displayArticle[] = displayArticles;
   constructor(
     private modalCtrol: ModalController,
   ) {
 
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  openCardModal(articleId:number) {
+  openCardModal(articleId: number) {
     // console.log("try edit activated");
     console.log(this.articleComponent[articleId].title);
-    // this.modalCtrol.create({
-    //   component: ArticleModalMainComponent,
-    //   componentProps: {
-    //     // id: this.articleId,
-    //     // title: this.contents[this.articleId].title,
-    //     // subtitle: this.contents[this.articleId].subtitle,
-    //     // image: this.contents[this.articleId].image,
-    //     // segment: JSON.parse(JSON.stringify(this.contents[this.articleId].segment)) 
-       
-    //   }
-    // }).then(modalres => {
-    //   modalres.present();
+    this.modalCtrol.create({
+      component: ArticleModalMainComponent,
+      componentProps: {
+        id: articleId,
+        title: this.contents[articleId].title,
+        subtitle: this.contents[articleId].subtitle,
+        image:this.contents[articleId].image,
+        cardIntroduction: this.contents[articleId].cardIntroduction
+      }
+    }).then(modalres => {
+      modalres.present();
 
-    //   modalres.onDidDismiss().then(res => {
-    //     if (res.data != null) {
-    //       this.articleComponent[this.articleId] = res.data;
-    //     }else{
-    //       console.log("no data ");
-    //     }
-    //   })
+      modalres.onDidDismiss().then(res => {
+        if (res.data != null) {
+          this.articleComponent[articleId] = res.data;
+        } else {
+          console.log("no data ");
+        }
+      })
 
-    // })
+    })
   }
 }
