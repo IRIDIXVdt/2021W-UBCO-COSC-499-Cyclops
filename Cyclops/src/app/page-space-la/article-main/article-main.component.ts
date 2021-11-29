344b522292fe1949c66ac690fc1a17248afb1eb8
IRIDIXVdt <larry_gu@126.com>
2021-11-30 00:26:14 +0800

    transfer article id to modal

--- Cyclops/src/app/page-space-la/article-main/article-main.component.ts
+++ Cyclops/src/app/page-space-la/article-main/article-main.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { displayArticle } from '../../sharedData/displayArticle';
+import { ArticleModalMainComponent } from '../article-modal-main/article-modal-main.component';
+import { ModalController } from '@ionic/angular';
+
 @Component({
   selector: 'app-article-main',
   templateUrl: './article-main.component.html',
@@ -9,11 +12,39 @@ export class ArticleMainComponent implements OnInit {
   @Input()
   articleComponent: displayArticle[];
 
-  constructor() { }
+  articleId;
+  constructor(
+    private modalCtrol: ModalController,
+  ) {
+
+  }
 
   ngOnInit() {}
 
-  openCardModal(){
-    console.log("card modal button click message");
+  openCardModal(articleId:number) {
+    // console.log("try edit activated");
+    console.log(this.articleComponent[articleId].title);
+    // this.modalCtrol.create({
+    //   component: ArticleModalMainComponent,
+    //   componentProps: {
+    //     // id: this.articleId,
+    //     // title: this.contents[this.articleId].title,
+    //     // subtitle: this.contents[this.articleId].subtitle,
+    //     // image: this.contents[this.articleId].image,
+    //     // segment: JSON.parse(JSON.stringify(this.contents[this.articleId].segment)) 
+       
+    //   }
+    // }).then(modalres => {
+    //   modalres.present();
+
+    //   modalres.onDidDismiss().then(res => {
+    //     if (res.data != null) {
+    //       this.articleComponent[this.articleId] = res.data;
+    //     }else{
+    //       console.log("no data ");
+    //     }
+    //   })
+
+    // })
   }
 }
