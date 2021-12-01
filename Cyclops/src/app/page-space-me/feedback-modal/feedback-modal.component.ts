5569dbc1cc4c9c17d021121af1baa00f1b416ca7
lilmergo <megan.nguyenn261@gmail.com>
2021-11-30 18:45:07 -0800

    Fix validation bugs

--- Cyclops/src/app/page-space-me/feedback-modal/feedback-modal.component.ts
+++ Cyclops/src/app/page-space-me/feedback-modal/feedback-modal.component.ts
@@ -1,7 +1,7 @@
-import { Component} from '@angular/core';
-import { ModalController,AlertController} from '@ionic/angular';
+import { Component } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Feedback , ContactType} from './feedback';
+import { Feedback, ContactType } from './feedback';
 @Component({
   selector: 'app-feedback-modal',
   templateUrl: './feedback-modal.component.html',
@@ -9,10 +9,10 @@ import { Feedback , ContactType} from './feedback';
 })
 export class FeedbackModalComponent {
   feedbackForm: FormGroup;
-  feedback:Feedback;
+  feedback: Feedback;
   contactType = ContactType;
 
-  constructor(private fb: FormBuilder, 
+  constructor(private fb: FormBuilder,
     public modalController: ModalController,
     public alertController: AlertController) {
     this.createForm();
@@ -40,41 +40,59 @@ export class FeedbackModalComponent {
     await alert.present();
   }
 
-  createForm(){
+  createForm() {
     this.feedbackForm = this.fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
       phoneNumber: [''],
       email: [''],
       agree: false,
-      contactType:'Email',
+      contactType: 'Email',
       content: ['', [Validators.required]]
     });
-    this.feedbackForm.get('agree').valueChanges.subscribe(value => {
-      if(value) {
-        if(this.feedbackForm.get('contactType').value==='Phone'||this.feedbackForm.get('contactType').value==='Both'){
-          this.feedbackForm.get('phoneNumber').setValidators(Validators.required);
-        }else if(this.feedbackForm.get('contactType').value==='Email'||this.feedbackForm.get('contactType').value==='Both'){
-          this.feedbackForm.get('email').setValidators(Validators.required);
-        }
-      } else {
+    this.feedbackForm.get('agree').valueChanges.subscribe(agreeValue => {
+      if (agreeValue === true) {
+        console.log(' first email required now');//if true, first set validator of email as required for the default option
+        this.feedbackForm.get('email').setValidators(Validators.required);
+        this.feedbackForm.get('contactType').valueChanges.subscribe(contactValue => {
+          console.log('mark all fields as untouched whenever there is a contact value change');
+          this.feedbackForm.get('email').markAsUntouched();
+          this.feedbackForm.get('phoneNumber').markAsUntouched();
+          if (contactValue === 'Email') {
+            console.log('add validator to email and remove phone validator');
+            this.feedbackForm.get('email').setValidators(Validators.required);
+            this.feedbackForm.get('phoneNumber').clearValidators();
+            this.feedbackForm.get('phoneNumber').updateValueAndValidity();
+          } else if (contactValue === 'Phone') {
+            console.log('add validator to phone and remove email validator');
+            this.feedbackForm.get('phoneNumber').setValidators(Validators.required);
+            this.feedbackForm.get('email').clearValidators();
+            this.feedbackForm.get('email').updateValueAndValidity();
+          } else if (contactValue === 'Both') {
+            console.log('add validator to email and phone validator');
+            this.feedbackForm.get('email').setValidators(Validators.required);
+            this.feedbackForm.get('phoneNumber').setValidators(Validators.required);
+          }
+        });
+      } else if (agreeValue === false) {
+        console.log('both email and number validator cleared ')
         this.feedbackForm.get('email').clearValidators();
-        this.feedbackForm.get('phoneNumber').clearValidators();
         this.feedbackForm.get('email').updateValueAndValidity();
+        this.feedbackForm.get('phoneNumber').clearValidators();
         this.feedbackForm.get('phoneNumber').updateValueAndValidity();
       }
     });
   }
 
 
-  ngOnInit(){
+  ngOnInit() {
 
-   }
+  }
 
-  onSolution(){
+  onSolution() {
     if (!this.feedbackForm.valid) {
       console.log('All fields are required.')
-      this.presentErrorAlert() ;
+      this.presentErrorAlert();
       return false;
     } else {
       this.feedback = this.feedbackForm.value;
@@ -85,10 +103,10 @@ export class FeedbackModalComponent {
     }
 
   }
-  
+
   dismissModal() {
     this.modalController.dismiss();
   }
-  
+
 
 }
