eb9f8107a32bbe3bb9bffc43caa95c5f58ca5794
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-01-16 00:45:40 +0800

    Select Article with Buttons for CKEditor

--- Cyclops/src/app/editing-tool-test-page/editing-tool-test-page.page.ts
+++ Cyclops/src/app/editing-tool-test-page/editing-tool-test-page.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
+import { ChangeEvent, CKEditorComponent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
 import { displayArticle } from '../sharedData/displayArticle';
 import { displayArticles } from '../sharedData/displayArticles';
 import { ActivatedRoute } from '@angular/router';
@@ -11,6 +11,8 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./editing-tool-test-page.page.scss'],
 })
 export class EditingToolTestPagePage implements OnInit {
+  @ViewChild( 'editor' ) editorComponent: CKEditorComponent;
+
   //get access to all the articles
   contents: displayArticle[] = displayArticles;
   //sepcify the segment section
@@ -32,7 +34,17 @@ export class EditingToolTestPagePage implements OnInit {
 
   //Import the editor build in your Angular component and assign it to a public property to make it accessible from the template
   public Editor = ClassicEditor;
+  public onChipClick(index:number){
+    console.log("change segment to new page "+index);
+    this.currentSeg=index;
+    // this.model.editorData= this.contents[this.articleId].segment[this.currentSeg].segmentBody;
+    // console.log(this.editorComponent.editorInstance);\
+    this.editorComponent.editorInstance.setData(this.contents[this.articleId].segment[this.currentSeg].segmentBody)
+  }
 
+  public onChipAdd(){
+    console.log("not implemented yet");
+  }
 
   public onChange( { editor }: ChangeEvent ) {
     const data = editor.getData();
