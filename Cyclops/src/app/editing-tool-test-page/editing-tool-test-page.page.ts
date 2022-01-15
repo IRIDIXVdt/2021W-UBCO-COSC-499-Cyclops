f3b28657a53eec942c4e3c22983beeaf25a1ead7
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-01-15 23:50:38 +0800

    Segment Section Button Initialized
    with routing issue bug fix

--- Cyclops/src/app/editing-tool-test-page/editing-tool-test-page.page.ts
+++ Cyclops/src/app/editing-tool-test-page/editing-tool-test-page.page.ts
@@ -13,23 +13,26 @@ import { ActivatedRoute } from '@angular/router';
 export class EditingToolTestPagePage implements OnInit {
   //get access to all the articles
   contents: displayArticle[] = displayArticles;
+  //sepcify the segment section
+  currentSeg: number;
   //get access to a specific article (with the provided id passes from page space la)
   articleId;
-  public model = {//model specifies the information the page would get
-    editorData: this.contents[0].segment[1].segmentBody
-  };
+  public model;
 
   constructor(
     private activatedrouter: ActivatedRoute
   ) {
-    this.articleId = this.activatedrouter.snapshot.paramMap.get('id');
+    this.currentSeg = 0;
     //fetch article id from the other side and store it in articleId
-
-    // this.
+    this.articleId = this.activatedrouter.snapshot.paramMap.get('id'); 
+    this.model = {//model specifies the information the page would get
+    editorData: this.contents[this.articleId].segment[this.currentSeg].segmentBody
+  };
   }
 
   //Import the editor build in your Angular component and assign it to a public property to make it accessible from the template
   public Editor = ClassicEditor;
+
 
   public onChange( { editor }: ChangeEvent ) {
     const data = editor.getData();
