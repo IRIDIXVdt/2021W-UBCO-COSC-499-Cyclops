6373bca0d00e52e41357de134546d08cdec1c644
Sumeet <sumeet-bajwa-34@live.ca>
2022-01-17 20:33:08 -0800

    Application to web sidebar
    Application to web ion-bar, but working on routing new ion-bar
    
    some issues being worked on

---
+++ Cyclops/src/app/services/screensize.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class ScreensizeService {
+ 
+  private isDesktop = new BehaviorSubject<boolean>(false);
+ 
+  constructor() { }
+ 
+  onResize(size) {    
+    console.log('size: ', size)
+    if (size < 768) {
+      this.isDesktop.next(false);
+    } else {
+      this.isDesktop.next(true);
+    }
+  }
+ 
+  isDesktopView(): Observable<boolean> {
+    return this.isDesktop.asObservable().pipe(distinctUntilChanged());
+  }
+}
--- Cyclops/src/app/tabs/tabs.page.ts
+++ Cyclops/src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScreensizeService } from '../services/screensize.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-
-  constructor() {}
-
+  isDesktop: boolean;
+ 
+  constructor(private screensizeService: ScreensizeService) {
+    this.screensizeService.isDesktopView().subscribe(isDesktop => {
+      if (this.isDesktop && !isDesktop) {
+        // Reload because our routing is out of place
+        window.location.reload();
+      }
+ 
+      this.isDesktop = isDesktop;
+    });
+  }
 }
