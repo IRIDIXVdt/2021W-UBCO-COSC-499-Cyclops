1d99dda8914eed834f100a15794d91deaf6bbcc5
Eric Jiang <970849953@qq.com>
2022-01-23 17:27:27 +0800

    edit demo page and get data from firestore for testing

--- Cyclops/src/app/crud-demo/crud-demo.page.ts
+++ Cyclops/src/app/crud-demo/crud-demo.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ServiceService} from '../service.service';
+import { FirebaseService } from '../firebase.service';
+import { ServiceService } from '../service.service';
+import { displayArticle } from '../sharedData/displayArticle';
+import { displayArticles } from '../sharedData/displayArticles';
+
 @Component({
   selector: 'app-crud-demo',
   templateUrl: './crud-demo.page.html',
@@ -8,36 +12,58 @@ import {ServiceService} from '../service.service';
 export class CRUDDemoPage implements OnInit {
   name: any;
   users: any;
+  displayArticle: any;
   constructor(
-    public service: ServiceService
-  ) {this.getName()}
+    public service: ServiceService,
+    public firebaseService: FirebaseService
+  ) {
+    this.firebaseService.getDataService().subscribe((res) => {
+      this.displayArticle = res.map(e => {
+        return {
+          // articleId: e.payload.doc.id,
+          articleId: e.payload.doc.data()['id'],
+          //in case this is reading document id, we read by doc.data instead
+          articleTitle: e.payload.doc.data()['title'],
+          articleSegment: e.payload.doc.data()['segment1']
+          //read document from firebase
+        }
+      })
+      console.log(this.displayArticle);
+    }, (err: any) => {
+      console.log(err);
+    })
 
-  addName(){
+
+
+  }
+  addName() {
     console.log(this.name);
     let data = {
       name: this.name
     }
 
-    this.service.addNameService(data).subscribe((res:any) =>{
-      console.log("SUCCESS ===",res);
-      this.name='';
+    this.service.addNameService(data).subscribe((res: any) => {
+      console.log("SUCCESS ===", res);
+      this.name = '';
       alert("SUCCESS");
       this.getName();
-    },(error:any) => {
-      console.log("ERROR ===",error);
+    }, (error: any) => {
+      console.log("ERROR ===", error);
     })
-  } 
+  }
 
-  getName(){
-    this.service.getNameService().subscribe((res:any) =>{
-      console.log("getName SUCCESS ===",res);
-      this.users=res;
-    },(error:any) => {
-      console.log("getName ERROR ===",error);
+  getName() {
+    this.service.getNameService().subscribe((res: any) => {
+      console.log("getName SUCCESS ===", res);
+      this.users = res;
+    }, (error: any) => {
+      console.log("getName ERROR ===", error);
     })
   }
 
   ngOnInit() {
   }
+
+
 
 }
--- Cyclops/src/app/firebase.service.ts
+++ Cyclops/src/app/firebase.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import{AngularFirestore} from '@angular/fire/compat/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
+  collectionName: 'displayArticles';
+  //.collection("displayArticles")
+  constructor(private firestore: AngularFirestore) {
+    
+   }
 
-  constructor() { }
+   getDataService(){
+    return this.firestore.collection("displayArticles").snapshotChanges();
+    
+  }
 }
+
+
