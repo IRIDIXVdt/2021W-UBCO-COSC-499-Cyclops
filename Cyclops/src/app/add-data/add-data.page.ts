81042dabd24707ffd35a0dd27c9ca6a4b638703f
Eric Jiang <970849953@qq.com>
2022-01-23 17:29:01 +0800

    Create the addData page

---
+++ Cyclops/src/app/add-data/add-data.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-data',
+  templateUrl: './add-data.page.html',
+  styleUrls: ['./add-data.page.scss'],
+})
+export class AddDataPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
