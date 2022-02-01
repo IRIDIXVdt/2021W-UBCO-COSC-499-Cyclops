9a16af9bda2a0975f1698114aeda645b3af8d697
Sumeet <sumeet-bajwa-34@live.ca>
2022-01-31 17:35:18 -0800

    New component
    generated new modal for star component

---
+++ Cyclops/src/app/page-space-su/star-modal/star-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-star-modal',
+  templateUrl: './star-modal.component.html',
+  styleUrls: ['./star-modal.component.scss'],
+})
+export class StarModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
