308a6502a50b79a14aa6ef17b160280166a16720
Sumeet <sumeet-bajwa-34@live.ca>
2022-02-06 13:12:15 -0800

    Update score-modal.component.ts
    updated score modal ts with correct methods that allow for rating change on leafs

--- Cyclops/src/app/page-space-su/score-modal/score-modal.component.ts
+++ Cyclops/src/app/page-space-su/score-modal/score-modal.component.ts
@@ -76,6 +76,48 @@ export class ScoreModalComponent implements OnInit  {
       this.modalCtrl.dismiss(this.usereco)
     }
   }
+  rate(index: number) {
+    // function used to change the value of our rating 
+    // triggered when user, clicks a star to change the rating
+    this.rating = index;
+    console.log(this.rating);
+    this.ratingChange.emit(this.rating);
+ }
+
+ 
+  getColor(index: number) {
+    /* function to return the color of a star based on what
+     index it is. All stars greater than the index are assigned
+     a grey color , while those equal or less than the rating are
+     assigned a color depending on the rating. Using the following criteria:
+  
+          1-2 stars: red
+          3 stars  : yellow
+          4-5 stars: green 
+    */
+   if(this.isAboveRating(index)){
+     return this.grey;
+   }
+   switch (this.rating){
+     case 1:
+     case 2:
+       return this.green;
+    case 3:
+      return this.green;
+    case 4:
+    case 5:
+      return this.green;
+    default:
+      return this.grey;  
+   }
+  }
+
+  isAboveRating(index: number): boolean {
+    // returns whether or not the selected index is above ,the current rating
+    // function is called from the getColor function.
+    return index > this.rating;
+  }
+
   
 
     
