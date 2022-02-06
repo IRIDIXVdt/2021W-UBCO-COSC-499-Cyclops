69af4e0ea450f43e9124bf5de13a9871146ad10c
Eric Jiang <970849953@qq.com>
2022-02-06 17:46:07 +0800

    add input checking and improvement on UI of login page

--- Cyclops/src/app/login/login.page.ts
+++ Cyclops/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -7,36 +8,48 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  /* email ="";
-  password="";
-  
-  currentUser:any;
-
-  focused: boolean;
-  focused2: boolean;
-
-  onBlur(event: any) {
-    const value = event.target.value;
-
-    if (!value) {
-      this.focused = false;
-    }
+  public errorMessages ={
+    email: [
+      {
+        type:'required', message:'Email is required.'
+      },
+      {
+        type:'pattern', message:'Please enter a valid email address.'
+      }
+    ],
+    password: [
+      {
+        type:'required', message:'Password is required.'
+      },
+      {
+        type:'minlength', message:'Your password should be more than 6 character.'
+      },
+      {
+        type:'maxlength', message:'Your password should be less than 20 character.'
+      },
+      /* {
+        type:'pattern', message:'Should contains at least one letter and one number.'
+      } */
+    ]
   }
 
-  onBlur2(event: any) {
-    const value2 = event.target.value;
-
-    if (!value2) {
-      this.focused2 = false;
-    }
-  } */
-
+  signInForm: FormGroup;
   constructor( public authService: AuthService) { 
-    /* if(this.afAuth.currentUser){
-      
-    } */
+    this.signInForm = new FormGroup({
+      formEmail: new FormControl('',[
+        Validators.required,
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      formPassword: new FormControl('',[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        
+      ])
+
+    });
+
   }
+  
 
   ngOnInit() {
   }
