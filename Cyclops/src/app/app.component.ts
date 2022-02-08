3d36e3faf28cfc1df2652142ba1b2d33b7bb2296
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-02-09 00:47:55 +0800

    routing information display

--- Cyclops/src/app/app.component.ts
+++ Cyclops/src/app/app.component.ts
@@ -1,5 +1,6 @@
 
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    console.log('this.router.url', this.router.url);
+  }
 }
 
