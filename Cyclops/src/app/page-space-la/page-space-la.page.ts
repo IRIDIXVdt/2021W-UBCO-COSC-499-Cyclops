278cc7341e3a99e54ee36b97f198eb72e37312a0
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-02-08 20:49:07 +0800

    Article Search Result Display
    bug fix

--- Cyclops/src/app/page-space-la/page-space-la.page.ts
+++ Cyclops/src/app/page-space-la/page-space-la.page.ts
@@ -15,25 +15,22 @@ export class PageSpaceLaPage implements OnInit {
   status1: any;
   dummySearchField: fetchArticle[];
   searchField: fetchArticle[];
-  contents: displayArticle[];
   segmentChanged(ev: any) {
     console.log('Segment changed', ev);
     console.log('current segment is', this.status1);
   }
 
-
-
   constructor(public firebaseService: FirebaseService) {
     this.status1 = "Articles p1";
-    // (document.querySelector('#requested') as HTMLElement).style.display = "none";
   }
 
-  async loadData() {
+  async loadData(searchbarComponent:HTMLElement) {
     // loadData loads all article information into the searchField Component
     this.firebaseService.getDataServiceMainPage().subscribe((res) => {
       this.searchField = res.map(e => {
         return {
           id: e.payload.doc.id,
+          columnName: e.payload.doc.data()['columnName'],
           title: e.payload.doc.data()['title'],
           subtitle: e.payload.doc.data()['subtitle'],
           segment: e.payload.doc.data()['segment'],
@@ -42,10 +39,8 @@ export class PageSpaceLaPage implements OnInit {
       })
       console.log("Search Field Loaded");
       console.log(this.searchField);
-
-      this.dummySearchField = this.searchField;
-      console.log('Display: dsf');
-      console.log(this.dummySearchField);
+      
+      searchbarComponent.style.display = "block";
     }, (err: any) => {
       console.log(err);
     })
@@ -53,39 +48,22 @@ export class PageSpaceLaPage implements OnInit {
   searchBarOnclick() {
     this.dummySearchField = this.searchField;
     console.log("clicked");
-    this.contents = displayArticles;
-    
   }
 
   ngOnInit() {
-    
-    this.loadData();
-
-    let items = null;
-    // disable event Listener
-
     const thisNotShow = document.querySelector('#requested') as HTMLElement;
     thisNotShow.style.display = 'none';
 
     const textSpace = document.querySelector('#origin') as HTMLElement;
     const searchbar = document.querySelector('ion-searchbar');
-    // function handleInput(event) {
-    //   items = Array.from(document.querySelector('ion-grid').children as HTMLCollectionOf<HTMLElement>);
-    //   const query = event.target.value.toLowerCase();
-    //   requestAnimationFrame(() => {
-    //     items.forEach(item => {
-    //       const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
-    //       item.style.display = shouldShow ? 'block' : 'none';
-    //     });
-    //   });
-    // }
-
+    searchbar.style.display = "none";
+    this.loadData(searchbar);
     searchbar.addEventListener('ionFocus', handleFocus);
     //searchbar focus, hide other things
     function handleFocus() {
       const searchResult = document.querySelector('#requested') as HTMLElement;
       console.log('message Focus emit');
-      items = Array.from(document.querySelector('ion-grid').children as HTMLCollectionOf<HTMLElement>);
+      // items = Array.from(document.querySelector('ion-grid').children as HTMLCollectionOf<HTMLElement>);
       requestAnimationFrame(() => {
         textSpace.style.display = 'none';
         searchResult.style.display = 'block';
@@ -112,4 +90,6 @@ type fetchArticle = {
   subtitle: string;
   cardIntroduction: string;
   segment: segmentItem[];
+  columnName: string;
+  //we still need the columnName for displaying, columnName less than 0 means it is deleted
 }
