6110a02ea2996df0bc71007193d9a03980ad2678
Eric Jiang <970849953@qq.com>
2022-02-16 00:51:29 +0800

    Edit userName

--- Cyclops/src/app/authentication/user-profile/user-profile.page.ts
+++ Cyclops/src/app/authentication/user-profile/user-profile.page.ts
@@ -13,20 +13,20 @@ export class UserProfilePage implements OnInit {
 
 
 
-  constructor(public authService: AuthService,public alertController: AlertController) { }
+  constructor(public authService: AuthService, public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
-  async editUsername(a) {
+  async editUsername() {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Edit User Name',
       inputs: [
         {
-          name: 'name1',
+          name: 'userName',
           type: 'text',
-          placeholder: 'Current UserName: '+a
+          placeholder: 'Current UserName: ' + this.userData.displayName
         },
       ],
       buttons: [
@@ -38,15 +38,44 @@ export class UserProfilePage implements OnInit {
             console.log('Confirm Cancel');
           }
         }, {
-          text: 'Ok',
-          handler: () => {
-            this.authService.updateUserName('name1');
-            console.log('Confirm Ok');
+          text: 'Save',
+          handler: data => {
+            if (this.validateUserName(data)) {
+              console.log("run-------------")
+              this.authService.updateUserName(data.userName);
+              this.userData = JSON.parse(localStorage.getItem('user'));
+            } else {
+              this.alertError('You have reached the max length 10');
+            }
           }
         }
       ]
     });
 
+    await alert.present();
+    console.log(this.userData.displayName);
+  }
+
+  validateUserName(data) {
+    if (false) {
+      return {
+        isValid: true,
+        message: ''
+      };
+    } else {
+      return {
+        isValid: false,
+        message: 'Email address is required'
+      }
+    }
+  }
+
+  async alertError(message){
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      message: message,
+      buttons: ['Ok']
+    });
     await alert.present();
   }
 
