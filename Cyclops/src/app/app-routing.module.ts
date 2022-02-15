b4832d585df76e8873bd15b4fd3d6243b22b2193
Eric Jiang <970849953@qq.com>
2022-02-15 17:00:54 +0800

    apply guard

--- Cyclops/src/app/app-routing.module.ts
+++ Cyclops/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {
@@ -62,8 +63,10 @@ const routes: Routes = [
   {
     path: 'reset-password',
     loadChildren: () => import('./authentication/reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
-  },  {
+  },
+  {
     path: 'user-profile',
+    canActivate:[AuthGuard],
     loadChildren: () => import('./authentication/user-profile/user-profile.module').then( m => m.UserProfilePageModule)
   }
 
--- Cyclops/src/app/guard/auth.guard.ts
+++ Cyclops/src/app/guard/auth.guard.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../authentication/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(public authService: AuthService){}
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot):boolean{
+    return this.authService.isLogin();
   }
   
 }
