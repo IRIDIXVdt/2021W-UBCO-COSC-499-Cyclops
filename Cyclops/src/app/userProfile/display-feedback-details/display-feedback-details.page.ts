ebc7f41062c6ef9edc56f0c2acc8a42483025cce
Eric Jiang <970849953@qq.com>
2022-02-16 19:20:36 +0800

    generate display feedback details page

---
+++ Cyclops/src/app/userProfile/display-feedback-details/display-feedback-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-feedback-details',
+  templateUrl: './display-feedback-details.page.html',
+  styleUrls: ['./display-feedback-details.page.scss'],
+})
+export class DisplayFeedbackDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
