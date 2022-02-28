24b34b6d69985fab28a4f08e38ccc95a13bd408f
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-02-28 22:07:15 +0800

    Image Update Implemented

--- Cyclops/src/app/page-space-la/article-image/article-image.page.ts
+++ Cyclops/src/app/page-space-la/article-image/article-image.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, NavParams } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController, NavParams } from '@ionic/angular';
 import { FirebaseService } from 'src/app/FirebaseService/firebase.service';
 
 @Component({
@@ -13,13 +13,46 @@ export class ArticleImagePage implements OnInit {
     public modalController: ModalController,
     private navParams: NavParams,
     public firebaseService: FirebaseService,
+    public loadingController: LoadingController,
+    public alertController: AlertController,
   ) {
     this.articleId = this.navParams.data.content;//fetch id from database
   }
 
   ngOnInit() {
   }
+  async clickImageEvent(name) {
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+    });
+    loading.present();  // present loading animation
+    const contents = {
+      image: name,
+    }
+    //set changes to local
+    this.firebaseService.updateDataByIdService(this.articleId, contents).then((res: any) => {
+      console.log("Changes saved to cloud!", res);//log success mesage to console
+      this.alertMess("Card Cover Change Success");//tell the user the success message
+      loading.dismiss();//close loading animation
+      //local saving
+      this.modalController.dismiss();//close modal
+      //remote saving complete
+    }).catch((error) => {
+      loading.dismiss();
+      this.alertMess('Failed to save changes, Try again!');
+      console.log("error", error);
+    })
 
+  }
+  async alertMess(message) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      subHeader: '',
+      message: message,
+      buttons: ['Ok']
+    });
+    await alert.present();
+  }
   dismissModal() {
     this.modalController.dismiss();
   }
@@ -27,12 +60,10 @@ export class ArticleImagePage implements OnInit {
   listP = [
     '../assets/climate.jpg',
     '../assets/crowd.jpg',
-    // '../assets/default-user-image.png',
     '../assets/effec.jpg',
     '../assets/field.jpg',
     '../assets/fog.jpg',
     '../assets/hope.jpg',
-    // '../assets/influence.jpg',
     '../assets/mine.jpg',
     '../assets/money.jpg',
     '../assets/pexels-eriks-cistovs-10002691.jpg',
