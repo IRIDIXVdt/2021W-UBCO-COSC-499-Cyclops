36a90c4063eaa43b926401c28ab0f3f5f06fc191
Sumeet <sumeet-bajwa-34@live.ca>
2022-03-10 15:49:10 -0800

    minor changes

--- Cyclops/src/app/page-space-su/score-modal/score-modal.component.ts
+++ Cyclops/src/app/page-space-su/score-modal/score-modal.component.ts
@@ -227,17 +227,12 @@ export class ScoreModalComponent implements OnInit  {
     this.starbuttonClicked = !this.starbuttonClicked;
     this.starsols = StarSolutions[0].starsols.find(s=> s.star == val).starsol;
   }
-  getStarsForSelectedSolution(val:string){
+  getStarsForSelectedSolution(val:string[]){
     // this.starsection = StarSolutions[0].sectionstars;
     // this.usereco.level= StarSolutions[0].sectionstars.find(s=> this.starsection = val.trim()).starinput;
 
-   
-   
-      this.usereco.level = StarSolutions[0].starsols.find(s=> s.starsol[0] == val).star;
+      this.usereco.level = StarSolutions[0].starsols.find(s=> this.starsol == val).star;
       console.log(this.usereco.level);
-   
-      // this.usereco.level = StarSolutions[0].starsols.find(s=> s.starsol[0] == val).star;
-      // console.log(this.usereco.level);
     
   }
  
