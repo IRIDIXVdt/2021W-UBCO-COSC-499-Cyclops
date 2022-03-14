4dbb67e5fd452453effbb245b245ceda4d5b0341
Sumeet <sumeet-bajwa-34@live.ca>
2022-03-14 14:54:49 -0700

    removed

--- Cyclops/src/app/page-space-su/score-modal/SectionSolution.ts
+++ Cyclops/src/app/page-space-su/score-modal/SectionSolution.ts
@@ -3,7 +3,6 @@ export const SectionSolution : SectionSolutionTags[] = [
 
     {
         sections : ["Transportation", "Food"],
-        sectionstars: [1,2,3,4,5],
     
         solution: [
             {
--- Cyclops/src/app/page-space-su/score-modal/SectionSolutionTags.ts
+++ Cyclops/src/app/page-space-su/score-modal/SectionSolutionTags.ts
@@ -1,6 +1,5 @@
 export class SectionSolutionTags{
     sections : string[];
-    sectionstars: number[];
     solution : {
         section : string;
         solutions : string[];
