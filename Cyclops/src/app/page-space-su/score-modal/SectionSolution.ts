c549f1ade8a431235a07f3e0064c48bff1c70e64
Sumeet <sumeet-bajwa-34@live.ca>
2022-03-14 15:49:42 -0700

    Update SectionSolution.ts

--- Cyclops/src/app/page-space-su/score-modal/SectionSolution.ts
+++ Cyclops/src/app/page-space-su/score-modal/SectionSolution.ts
@@ -9,18 +9,52 @@ export const SectionSolution : SectionSolutionTags[] = [
                 section: "Food",
                 scores: [1,2,3,4,5],
                 staramount:[{
-                    amount: 1,
-                    amountsolution: ["food1","food2"],
-                }
+                        amount: 1,
+                        amountsolution: ["food1","1"],
+                    },
+                    {
+                        amount: 2,
+                        amountsolution: ["food2","2"],
+                    },
+                    {
+                        amount: 3,
+                        amountsolution: ["food3","3"],
+                    },
+                    {
+                        amount: 4,
+                        amountsolution: ["food4","4"],
+                    },
+                    {
+                        amount: 5,
+                        amountsolution: ["food5","5"],
+                    },
+                
                 ]
             },
             {
                 section: "Transportation",
                 scores: [1,2,3,4,5],
                 staramount:[{
-                    amount: 1,
-                    amountsolution: ["transportation1","transportation2"],
-                }
+                        amount: 1,
+                        amountsolution: ["transportation1","1"],
+                    },
+                    {
+                        amount: 2,
+                        amountsolution: ["transportation2","2"],
+                    },
+                    {
+                        amount: 3,
+                        amountsolution: ["transportation3","3"],
+                    },
+                    {
+                        amount: 4,
+                        amountsolution: ["transportation4","4"],
+                    },
+                    {
+                        amount: 5,
+                        amountsolution: ["transportation5","5"],
+                    },
+                
                 ]
             }
         ]
