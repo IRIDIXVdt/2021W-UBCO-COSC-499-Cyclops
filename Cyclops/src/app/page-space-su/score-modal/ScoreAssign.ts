de71977d93bc355bf6bc7ecf1dec47089b5e7227
Sumeet <sumeet-bajwa-34@live.ca>
2022-03-16 17:52:32 -0700

    updated format for ts file

--- Cyclops/src/app/page-space-su/score-modal/ScoreAssign.ts
+++ Cyclops/src/app/page-space-su/score-modal/ScoreAssign.ts
@@ -1,48 +1,48 @@
 export type ScoreAssign = {
-	starnumber: number;
 	solutionname: string;
+    starnumber: number;
 };
 
 
 export const ScoreAssign: ScoreAssign[] = [
 	{
-		starnumber: 1,
-		solutionname: "Reduce meat consumption"
+		solutionname: "Reduce meat consumption",
+        starnumber: 1
 	},
 	{
-		starnumber: 1,
-		solutionname: "Forget about the labels"
+		solutionname: "Forget about the labels",
+        starnumber: 1
 	},
 	{
-		starnumber: 2,
-		solutionname: "Try New things"
+		solutionname: "Try New things",
+        starnumber: 2
 	},
 	{
-		starnumber: 2,
-		solutionname: "Find your high impact food"
+		solutionname: "Find your high impact food",
+        starnumber: 2
 	},
 	{
-		starnumber: 3,
-		solutionname: "Live carlessly"
+		solutionname: "Live carlessly",
+        starnumber: 3
 	},
     {
-		starnumber: 3,
-		solutionname: "Reduce car use"
+		solutionname: "Reduce car use",
+        starnumber: 3
 	},
     {
-		starnumber: 4,
-		solutionname: "Buy low emission vehicle"
+		solutionname: "Buy low emission vehicle",
+        starnumber: 4
 	},
     {
-		starnumber: 4,
-		solutionname: "Fill your car seats when you ride"
+		solutionname: "Fill your car seats when you ride",
+        starnumber: 4
 	},
     {
-		starnumber: 5,
-		solutionname: "star5solution"
+		solutionname: "star5solution",
+        starnumber: 5
 	},
     {
-		starnumber: 5,
-		solutionname: "Remember that everything a car powers depends on burning gasoline"
+		solutionname: "Remember that everything a car powers depends on burning gasoline",
+        starnumber: 5
 	}
 ];
