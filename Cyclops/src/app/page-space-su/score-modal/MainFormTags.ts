ed243a7bf28a10fb9f623c5cd7acc899d35ab9f1
Sumeet <sumeet-bajwa-34@live.ca>
2022-03-17 17:33:38 -0700

    Update MainFormTags.ts

--- Cyclops/src/app/page-space-su/score-modal/MainFormTags.ts
+++ Cyclops/src/app/page-space-su/score-modal/MainFormTags.ts
@@ -1,10 +1,12 @@
 export type MainFormTags = {
 	stars: number[];
+	solutions: string[];
 };
 
 export const MainFormTags: MainFormTags[] = [
 	{
-        stars: [1,2,3,4,5]
+        stars: [1,2,3,4,5],
+		solutions: ["Transportation", "Food"]
 	
 	}
 ];
