3157a1903fa93ea0bea7d85228058090fc2bcc92
Sumeet <sumeet-bajwa-34@live.ca>
2022-03-20 19:31:50 -0700

    changes

--- Cyclops/src/app/page-space-su/score-modal/score-modal.component.ts
+++ Cyclops/src/app/page-space-su/score-modal/score-modal.component.ts
@@ -197,18 +197,18 @@ export class ScoreModalComponent implements OnInit  {
     this.usereco.range = ($event.target.value); //Value to database
     console.log(this.usereco.range);
 
-    if (this.usereco.range == 2) {
-      this.color = 'success';
-    }
-    else if (this.usereco.range == 1) {
-        this.color = 'warning';
-    }
-    else if (this.usereco.range == 0) {
-        this.color = 'medium';
-    }
-    else if (this.usereco.range == -1)  {
-        this.color = 'danger';
-    }
+    // if (this.usereco.range == 2) {
+    //   this.color = 'success';
+    // }
+    // else if (this.usereco.range == 1) {
+    //     this.color = 'warning';
+    // }
+    // else if (this.usereco.range == 0) {
+    //     this.color = 'medium';
+    // }
+    // else if (this.usereco.range == -1)  {
+    //     this.color = 'danger';
+    // }
 
     if(this.usereco.range == 2){
       this.usereco.updatedscore = (this.usereco.level*this.usereco.range)/2;
