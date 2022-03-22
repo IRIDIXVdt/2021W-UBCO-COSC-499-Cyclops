6326197562e6c155a918f49f7a74ad2e39cd449b
Sumeet <sumeet-bajwa-34@live.ca>
2022-03-21 17:07:45 -0700

    updated scss

---
+++ Cyclops/src/app/page-space-su/solutionform/ScoreAssign.ts
@@ -0,0 +1,48 @@
+export type ScoreAssign = {
+	solutionname: string;
+    starnumber: number;
+};
+
+
+export const ScoreAssign: ScoreAssign[] = [
+	{
+		solutionname: "Reduce meat consumption",
+        starnumber: 1
+	},
+	{
+		solutionname: "Forget about the labels",
+        starnumber: 1
+	},
+	{
+		solutionname: "Try New things",
+        starnumber: 2
+	},
+	{
+		solutionname: "Find your high impact food",
+        starnumber: 2
+	},
+	{
+		solutionname: "Live carlessly",
+        starnumber: 3
+	},
+    {
+		solutionname: "Reduce car use",
+        starnumber: 3
+	},
+    {
+		solutionname: "Buy low emission vehicle",
+        starnumber: 4
+	},
+    {
+		solutionname: "Fill your car seats when you ride",
+        starnumber: 4
+	},
+    {
+		solutionname: "star5solution",
+        starnumber: 5
+	},
+    {
+		solutionname: "Remember that everything a car powers depends on burning gasoline",
+        starnumber: 5
+	}
+];
---
+++ Cyclops/src/app/page-space-su/solutionform/solutionform.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solutionform',
+  templateUrl: './solutionform.component.html',
+  styleUrls: ['./solutionform.component.scss'],
+})
+export class SolutionformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
