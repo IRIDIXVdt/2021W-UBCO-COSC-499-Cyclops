ed01b7e75ef9b3bc56b8a88801ddde012ba1f352
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-03-29 20:52:28 +0800

    Local Score Update

--- Cyclops/src/app/page-space-su/eco-edit/eco-edit.page.ts
+++ Cyclops/src/app/page-space-su/eco-edit/eco-edit.page.ts
@@ -49,8 +49,36 @@ export class EcoEditPage implements OnInit {
     await alert.present();
   }
 
-  save() {
-    console.log('save changes to cloud!');
+  async save() {
+    console.log('save changes to cloud!',this.ecoId);
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      message: 'Do you want to save all the changes to cloud?',
+      buttons: ['Cancel', 'Yes']
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    if (role == "cancel" || role == "backdrop") {
+      console.log("cancel!");
+    } else {
+      const loading = await this.loadingController.create({
+        message: 'Please wait...',
+      });
+      loading.present();  // present loading animation
+      this.firebaseService.updateEcoSolutionService(this.ecoId, this.solutionDetail).then((res: any) => {
+        console.log("Changes saved to cloud!", res);//log success mesage to console
+        this.alertMess("Upload Success");//tell the user the success message
+        loading.dismiss();//close loading animation
+        //local saving
+        this.modalController.dismiss();//close modal
+        //remote saving complete
+      }).catch((error) => {
+        loading.dismiss();
+        this.alertMess('Failed to save changes, Try again!');
+        console.log("error", error);
+      })
+
+    }
   }
 
   ngOnInit() {
