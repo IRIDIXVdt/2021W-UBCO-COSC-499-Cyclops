f9fc17de3b4eab61696b65451f45f4940282aea3
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-04-07 22:38:55 +0800

    Solution Addition Page Component Initialized

---
+++ Cyclops/src/app/page-space-su/eco-add/eco-add.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eco-add',
+  templateUrl: './eco-add.page.html',
+  styleUrls: ['./eco-add.page.scss'],
+})
+export class EcoAddPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
