0bba5b198eca163f8e4ef88dbcd34a3cd2bafad0
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-04-07 23:18:16 +0800

    Eco Solution Upload

--- Cyclops/src/app/page-space-su/eco-add/eco-add.page.ts
+++ Cyclops/src/app/page-space-su/eco-add/eco-add.page.ts
@@ -33,6 +33,17 @@ export class EcoAddPage implements OnInit {
     }
   }
 
+  async alertMess(message) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      subHeader: '',
+      message: message,
+      buttons: ['Ok']
+    });
+    await alert.present();
+  }
+
+
   ngOnInit() {
     this.solutionDetail = {
       name: 'solution name',
@@ -47,8 +58,48 @@ export class EcoAddPage implements OnInit {
     this.modalController.dismiss();
   }
 
-  save() {
+  async save() {
     console.log('save to cloud',this.solutionDetail);
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      message: 'Do you want to save this solution to cloud?',
+      buttons: ['Cancel', 'Yes']
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    if (role == "cancel" || role == "backdrop") {
+      console.log("cancel!");
+    } else {
+      const loading = await this.loadingController.create({
+        message: 'Please wait...',
+      });
+      loading.present();  // present loading animation
+
+
+      // this.firebaseService.updateEcoSolutionService(this.ecoId, this.solutionDetail).then((res: any) => {
+      //   console.log("Changes saved to cloud!", res);//log success mesage to console
+      //   this.alertMess("Upload Success");//tell the user the success message
+      //   loading.dismiss();//close loading animation
+      //   //local saving
+      //   this.modalController.dismiss();//close modal
+      //   //remote saving complete
+      // }).catch((error) => {
+      //   loading.dismiss();
+      //   this.alertMess('Failed to save changes, Try again!');
+      //   console.log("error", error);
+      // })
+
+      this.firebaseService.addDataService("NewEcoSolution", this.solutionDetail).then((res: any) => {
+        console.log(res);
+        loading.dismiss();
+        this.modalController.dismiss();
+      }).catch((error) => {
+        console.log(error);
+        this.alertMess('Failed to save changes, Try again!');
+        loading.dismiss();
+        this.modalController.dismiss();
+      })
+    }
   }
 
 }
