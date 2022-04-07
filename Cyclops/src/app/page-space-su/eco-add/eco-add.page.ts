818cc31cfeb3eafca0e7b7b04cf6f872a6d4c12d
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-04-07 22:48:01 +0800

    Eco solution addition modal control implemented

--- Cyclops/src/app/page-space-su/eco-add/eco-add.page.ts
+++ Cyclops/src/app/page-space-su/eco-add/eco-add.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, LoadingController, ModalController, NavParams } from '@ionic/angular';
+import { FirebaseService } from 'src/app/FirebaseService/firebase.service';
 
 @Component({
   selector: 'app-eco-add',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eco-add.page.scss'],
 })
 export class EcoAddPage implements OnInit {
+  solutionDetail: any;
+  constructor(
+    public modalController: ModalController,
+    public firebaseService: FirebaseService,
+    public alertController: AlertController,
+    public loadingController: LoadingController,
+  ) {
 
-  constructor() { }
+
+  }
 
   ngOnInit() {
+    this.solutionDetail = {
+      name: 'solution name',
+      detail: 'detail information',
+      section: 'test section',
+      star: 0,
+    }
+  }
+
+  dismissModal() {
+    this.modalController.dismiss();
+  }
+  save() {
+    console.log('save to cloud');
   }
 
 }
