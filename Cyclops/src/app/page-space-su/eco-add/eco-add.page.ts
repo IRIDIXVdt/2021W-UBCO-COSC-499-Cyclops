8203b458835a746930f23bcd408ea1e6a3a6ddba
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2022-04-07 23:07:28 +0800

    Eco Solution Information Display

--- Cyclops/src/app/page-space-su/eco-add/eco-add.page.ts
+++ Cyclops/src/app/page-space-su/eco-add/eco-add.page.ts
@@ -9,14 +9,28 @@ import { FirebaseService } from 'src/app/FirebaseService/firebase.service';
 })
 export class EcoAddPage implements OnInit {
   solutionDetail: any;
+  selectionList: any;
   constructor(
     public modalController: ModalController,
     public firebaseService: FirebaseService,
     public alertController: AlertController,
     public loadingController: LoadingController,
-  ) {
+  ) { }
 
+  getSections() {
 
+    const subscription = this.firebaseService.getSectionList().subscribe((res) => {
+      this.selectionList = res.map(e => {
+        return e.payload.doc.data()['sectionName'];
+      });
+      this.solutionDetail.section = this.selectionList[0];
+      console.log(this.selectionList);
+    }, (err: any) => {
+      console.log("Get section list error")
+    })
+    if (this.selectionList != null) {
+      subscription.unsubscribe();
+    }
   }
 
   ngOnInit() {
@@ -26,13 +40,15 @@ export class EcoAddPage implements OnInit {
       section: 'test section',
       star: 0,
     }
+    this.getSections();
   }
 
   dismissModal() {
     this.modalController.dismiss();
   }
+
   save() {
-    console.log('save to cloud');
+    console.log('save to cloud',this.solutionDetail);
   }
 
 }
